9e33c398561f5980b34a309a234dab552f5c2dac
Ilian-Yanev <59726635+Ilian-Yanev@users.noreply.github.com>
2020-10-20 20:24:59 +0300

    Make the basic structure of the menu

---
+++ Sets project/PRESENTATION_LAYER/Menu.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <studio.h>
+
+using namespace std;
+
+void Menu()
+{
+
+	int choice;
+
+    cout << "_________________________________________________________" << endl;
+    cout << endl;
+
+    cout << "+-------------------------------------------------------+" << endl;
+    cout << "|               Please, choose an action                |" << endl;
+    cout << "|                                                       |" << endl;
+    cout << "|                    1. Merge                           |" << endl;
+    cout << "|                    2. Section                         |" << endl;
+    cout << "|                    3. The difference (A\B)            |" << endl;
+    cout << "|                    4. The difference (B\A)            |" << endl;
+    cout << "|                    5. Symetrical difference           |" << endl;
+    cout << "|                    6. Subset                          |" << endl;
+    cout << "|                    9. Exit                            |" << endl;
+    cout << "+-------------------------------------------------------+" << endl;
+
+	cin >> choice;
+
+    switch (choice) {
+    case 1:
+        // 
+        break;
+
+    case 2:
+        // 
+        break;
+
+    case 3:
+        //
+        break;
+
+    case 4:
+        //
+        break;
+
+    case 5:
+        //
+        break;
+
+    case 9:
+        break;
+    
+    }
+}
