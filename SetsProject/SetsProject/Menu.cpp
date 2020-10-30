8f2556cfcf24626f6ad20a0815683a007b65ae23
Ilian-Yanev <59726635+Ilian-Yanev@users.noreply.github.com>
2020-10-30 17:11:56 +0300

    making the menu repeatable

--- SetsProject/SetsProject/Menu.cpp
+++ SetsProject/SetsProject/Menu.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 #include "Data.h"
 
-
 using namespace std;
 
 void VisualizationMerge()
@@ -91,68 +90,69 @@ void Subset()
 void Menu()
 {
     int choice;
+    do {
+        cout << endl;
 
-    cout << endl;
+        cout << "        +-------------------------------------+" << endl;
+        cout << "        |                                     |" << endl;
+        cout << "        |             Sets Project            |" << endl;
+        cout << "        |                                     |" << endl;
+        cout << "        +-------------------------------------+" << endl;
 
-    cout << "        +-------------------------------------+" << endl;
-    cout << "        |                                     |" << endl;
-    cout << "        |             Sets Project            |" << endl;
-    cout << "        |                                     |" << endl;
-    cout << "        +-------------------------------------+" << endl;
+        cout << "_________________________________________________________" << endl;
+        cout << endl;
 
-    cout << "_________________________________________________________" << endl;
-    cout << endl;
+        cout << "+-------------------------------------------------------+" << endl;
+        cout << "|               Please, choose an action                |" << endl;
+        cout << "|                                                       |" << endl;
+        cout << "|                    1. Merge                           |" << endl;
+        cout << "|                    2. Section                         |" << endl;
+        cout << "|                    3. The difference (A\B)             |" << endl;
+        cout << "|                    4. The difference (B\A)             |" << endl;
+        cout << "|                    5. Symetrical difference           |" << endl;
+        cout << "|                    6. Subset                          |" << endl;
+        cout << "|                    9. Exit                            |" << endl;
+        cout << "+-------------------------------------------------------+" << endl;
 
-    cout << "+-------------------------------------------------------+" << endl;
-    cout << "|               Please, choose an action                |" << endl;
-    cout << "|                                                       |" << endl;
-    cout << "|                    1. Merge                           |" << endl;
-    cout << "|                    2. Section                         |" << endl;
-    cout << "|                    3. The difference (A\B)             |" << endl;
-    cout << "|                    4. The difference (B\A)             |" << endl;
-    cout << "|                    5. Symetrical difference           |" << endl;
-    cout << "|                    6. Subset                          |" << endl;
-    cout << "|                    9. Exit                            |" << endl;
-    cout << "+-------------------------------------------------------+" << endl;
+        cout << "Choose an option: ";
+        cin >> choice;
 
-    cout << "Choose an option: ";
-    cin >> choice;
+        cout << "_________________________________________________________" << endl;
 
-    cout << "_________________________________________________________" << endl;
-
-    switch (choice)
-    {
-    case 1:
-        outputMerge();
-        VisualizationMerge();
-        break;
-
-    case 2:
-        outputSecton();
-        VisualizationSection();
-        break;
-
-    case 3:
-        //
-        DifferenceA();
-        break;
-
-    case 4:
-        //
-        DifferenceB();
-        break;
-
-    case 5:
-        //
-        SymetricalDifference();
-        break;
-
-    case 6:
-        //
-        Subset();
+        switch (choice)
+        {
+        case 1:
+            outputMerge();
+            VisualizationMerge();
             break;
 
-    case 9:
-        break;
-    }
+        case 2:
+            outputSecton();
+            VisualizationSection();
+            break;
+
+        case 3:
+            //
+            DifferenceA();
+            break;
+
+        case 4:
+            //
+            DifferenceB();
+            break;
+
+        case 5:
+            //
+            SymetricalDifference();
+            break;
+
+        case 6:
+            //
+            Subset();
+            break;
+
+        case 9:
+            break;
+        }
+    } while (choice != 9);
 }
